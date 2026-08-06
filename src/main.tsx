f685452b62e116783b0bf27f67047775c025ace8
dulaing <dulain.gunawardhana@ascentic.se>
2026-08-06 10:29:23 +0530

    feat: add TanStack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router'
+import {QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
+import {queryClient} from './app/queryClient.ts'
+import {worker} from './mocks/browser.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+async function startApp(): Promise<void> {
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+
+  const rootElement = document.getElementById('root');
+
+  if(!rootElement) {
+    throw new Error('Root element not found');
+  }
+
+  createRoot(rootElement).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+    </StrictMode>
+  )
+}
+
+void startApp();
